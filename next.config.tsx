f3d308bae0578c678265ac51db0ef2db27a44f12
tbatchelder <pegacorn@optonline.net>
2025-03-15 00:45:30 -0400

    Start of initial build

---
+++ next.config.tsx
@@ -0,0 +1,13 @@
+// /** @type {import("next").NextConfig} */
+
+// const nextConfig = {
+//   output: "export",
+//   distDir: "dist",
+//   images: {
+//     unoptimized: true,
+//   },
+//   basePath: "",
+//   trailingSlash: true,
+// };
+
+// module.exports = nextConfig;
